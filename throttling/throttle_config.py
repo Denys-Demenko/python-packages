14bc5405cba2d340e5f7bcd453a75d0870d303c2
Denys Demenko <d.demenko@foodtech.team>
2025-07-12 00:44:38 +0100

    fixed throttle config

--- throttling/throttle_config.py
+++ throttling/throttle_config.py
@@ -1,9 +1,9 @@
-from pydantic import Field
+from pydantic import Field, BaseModel
 
 from packages.mixins.from_to_mixin import FromToMixin
 
 
-class ThrottleConfig(FromToMixin):
+class ThrottleConfig(BaseModel, FromToMixin):
     rps: int = Field(..., description="Requests per second limit")
     rpm: int = Field(..., description="Requests per minute limit")
     rpd: int = Field(..., description="Requests per day limit")
