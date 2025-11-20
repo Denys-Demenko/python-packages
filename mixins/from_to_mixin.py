f83b09326761bef6b9d68f6902fd085c1e8f86b2
Denys Demenko <d.demenko@foodtech.team>
2025-11-20 18:06:38 +0100

    extra to to_dict

--- mixins/from_to_mixin.py
+++ mixins/from_to_mixin.py
@@ -2,6 +2,7 @@ from typing import Type, TypeVar
 from sqlalchemy.inspection import inspect
 
 T = TypeVar("T")
+DB = TypeVar("DB")
 
 
 class FromToMixin:
@@ -17,7 +18,7 @@ class FromToMixin:
     def from_db(cls: Type[T], db_obj) -> T:
         return cls.from_dict(FromToMixin.to_dict(db_obj))
 
-    def to_dict(self) -> dict:
+    def to_dict(self, **extra) -> dict:
         # Check if this is a SQLAlchemy model
         if hasattr(self, '__table__'):
             # SQLAlchemy model
@@ -27,6 +28,15 @@ class FromToMixin:
                 val = getattr(self, column.name)
                 if not column.primary_key or val is not None:
                     result[column.name] = val
-            return result
-        # Regular object
-        return {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
+        else:
+            # Regular object
+            result = {k: v for k, v in self.__dict__.items() if not k.startswith('_')}
+        
+        # Add extra key-value pairs
+        result.update(extra)
+        return result
+    
+    def to_db(self, db_class: Type[DB], **extra) -> DB:
+        """Конвертирует в модель БД"""
+        data = self.to_dict(**extra)
+        return db_class(**data)
